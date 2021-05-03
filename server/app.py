f7ad06088ab6e2dae78c59e2b343fca1d03a8096
bpbushman <brianprestonbushman@gmail.com>
2021-05-03 17:28:16 -0600

    added hello world

--- server/app.py
+++ server/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello World'
+    return '<h1>Hello World</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
